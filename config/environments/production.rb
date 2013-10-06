5847d07f931415a18904c3d5a7a462a83478e9d1
Amit Sonawane <amitvsonawane@gmail.com>
2013-10-05 21:21:04 -0300

    changed Devise views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # required for Heroku
+  #Note to set this to your actualy host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
